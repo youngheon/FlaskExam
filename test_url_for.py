e91944e9a35ad5f7906ed433ad078d3e926c26d8
youngheon <hewillon@gmail.com>
2014-11-27 21:50:10 +0900

    request, response, cookie,session

--- test_url_for.py
+++ test_url_for.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def hello():
 	return 'Hello Flask!'
 
+@app.route('/profile/',methods=['POST','GET'])
+def profile(username=None):
+	error = None
+	if request.method == 'POST':
+		username = request.form['username']
+		email = request.form['email']
+		if not username and not email:
+				return add_profile(request.form)
+	else:
+		error = 'Invalid username or email'
+	return  render_template('profile.html',error=error)
+
 @app.route('/profile/<username>')
 def get_profile(username):
 	return 'profile : ' + username
@@ -15,3 +27,18 @@ if __name__=='__main__':
 with app.test_request_context():
 	print url_for('hello')
 	print url_for('get_profile',username='flask')
+
+
+#쿠키와 세션
+@app.route('/')
+def index():
+	username=request.cookies.get('username')
+
+@app.route('/')
+def index():
+	resp = make_respone(render_template('hello.html'))
+	resp.set_cookie('username','flask')
+	return resp
+
+#/static/style.css
+url_for('static',filename='style.css')
